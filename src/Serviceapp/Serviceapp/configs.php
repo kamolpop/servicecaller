930618cf054628436a790c5f2ba415444f6cd2cf
kamolpop <kamolpop@thinknet.co.th>
2014-11-20 15:47:53 +0700

    test

--- src/Serviceapp/Serviceapp/configs.php
+++ src/Serviceapp/Serviceapp/configs.php
@@ -8,32 +8,66 @@ return array(
 		 * HTTP is default 
 		 */
 		
-		'protocal' 		=> Config::get('serviceapp.protocal'),
+		'protocal' 		=> Config::get('protocal'), 
+
+		/**
+		 * Connection default
+		 */
+		'default_host'	=> Config::get('default_host'),
 
 		/**
 		 * Domain name of service request
 		 */
 		
-		'host' 			=> Config::get('serviceapp.host'), 
+		'host' 			=>  Config::get('host'), 
 
 		/**
 		 * Client ID 
 		 */
 
-		'client_id' 	=> Config::get('serviceapp.client_id'), 
+		'client_id' 	=> Config::get('client_id'), 
 
 		/**
 		 * Secret Key use for indentify application. (Keep secret)
 		 */
 
-		'public_key' 	=> Config::get('serviceapp.public_key'), 
+		'public_key' 	=> Config::get('public_key'),
 
 		/**
 		 * Authentication server must take full path URL
 		 */
 
-		'authHost'		=> Config::get('serviceapp.authHost'), 
-	
-		);
+		'authHost'		=> Config::get('authHost'),
+
+
+
+		/**
+		* 	 ----------------- Access log configuration --------------------
+		*	|																|
+		*	|	1 . mongoDB storage											|
+		*	|	2 . send Alert with e-mail									|
+		*	|	3 . Enable domain list										|
+		*	|																|
+		*	 ---------------------------------------------------------------
+		*/
+
+		/**
+		* MongoDB config
+		*/
+		'LogDB'			=>	Config::get('LogDB'),
+
+		/**
+		 * Alert e-mail config
+		 */
+
+		'alert-mail' 	=> 	Config::get('alert-mail'),
+
+		/**
+		 * Enable log function domain list
+		 */
+
+		'EnableList'	=>	Config::get('EnableList'),
+
+	);
 
 ?>
